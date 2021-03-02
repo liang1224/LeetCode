c98c7030c86a896bed988a369567f6439c436b4f
liangjw <liangjw1224@gmail.com>
2021-03-02 14:54:27 +0800

    sync

--- longestPalindrome_test.go
+++ longestPalindrome_test.go
@@ -1,1 +1,21 @@
 package LeetCode
+
+import "testing"
+
+func TestLongestPalindrome1(t *testing.T) {
+	s := "abcdeftgdgtcace"
+	ans := longestPalindrome1(s)
+
+	if "tgdgt" != ans {
+		t.Error(ans)
+	}
+}
+
+func TestLongestPalindrome2(t *testing.T) {
+	s := "abcdeftgdgtcace"
+	ans := longestPalindrome2(s)
+
+	if "tgdgt" != ans {
+		t.Error(ans)
+	}
+}
